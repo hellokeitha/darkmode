ecc11a408251241be5215ee27f73cd3a47fdeba4
YJ Baek <yjbaek29@gmail.com>
2023-07-31 00:53:58 +0900

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+// src/App.tsx
 
-function App() {
+import React from "react";
+import DarkModeToggle from "./components/DarkModeToggle";
+import styled from "styled-components";
+
+const Container = styled.div`
+  background-color: ${(props) => props.theme.colors.background};
+  color: ${(props) => props.theme.colors.text};
+  height: 100vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+`;
+
+const App: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <h1>Hello, Dark Mode!</h1>
+      <DarkModeToggle />
+    </Container>
   );
-}
+};
 
 export default App;
---
+++ src/theme.ts
@@ -0,0 +1,15 @@
+const lightTheme = {
+  colors: {
+    background: "#ffffff",
+    text: "#000000",
+  },
+};
+
+const darkTheme = {
+  colors: {
+    background: "#222222",
+    text: "#ffffff",
+  },
+};
+
+export { lightTheme, darkTheme };
